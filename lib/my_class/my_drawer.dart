6be0fe988612bcad9626c284497b36fa0df74289
schezo817 <95132440+schezo817@users.noreply.github.com>
2022-03-04 19:17:05 +0900

    既存アニメーションのページ遷移メソッドの追加

---
+++ lib/my_class/my_drawer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Drawer{
+  /*
+  画面遷移するメソッド(片瀬)
+  使い方:
+  contextはbuildのcontextをそのまま入れる
+  pagesは遷移先のページを入れる。
+  ※Androidやiosの遷移をそのまま使っているので、アニメーションを変更したい場合はPageRouteBuilderを使ったページ遷移メソッドを作ること。
+   */
+  static Future movePage(BuildContext context, var pages) async {
+    return await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => pages,
+      ),
+    );
+  }
+
+}
