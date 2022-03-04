a1127c59c0c0db958560c7aeb8f651836da6077c
schezo817 <95132440+schezo817@users.noreply.github.com>
2022-03-04 21:25:01 +0900

    ver0.0.3 share_plusの問題解決とsubjectメッセージの変更

--- lib/my_class/my_share.dart
+++ lib/my_class/my_share.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
 
 class MyShare {
-  static void _share() => Share.share('みんなも教科書を売買しよう!',subject: '教科書売買');
+  static void _share() => Share.share('みんなも教科書を売買しよう!',subject: 'おすすめの教科書売買アプリ!');
 
   static Widget myShare(){
     return IconButton(
